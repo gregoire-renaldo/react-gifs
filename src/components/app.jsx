015a96a3f49e0dd16c365bb8a57f75715e3f5362
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-09-08 16:32:31 +0200

    selected gif

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
 
 class App extends Component {
+constructor(props) {
+  super(props);
+
+  this.state = {
+    gifs: [],
+    selectedGifId: "VIfE4DE7vY49i"
+  };
+}
+
+search = (query) => {
+  giphy('tjcPV5VbG1AKmoPNZpq9SaNZb29K9OQP').search({
+    q: query,
+    rating: 'g',
+    limit: 10
+  },  (error, result) => {
+    this.setState({
+      gifs: result.data
+    });
+  });
+}
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
   render() {
-    const gifs = [
-      { id: "VIfE4DE7vY49i" },
-      { id: "VKP6oCpyHqWWI" },
-      { id: "xUPGGdROwKhwyitUYw"}
-    ]
     return(
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id="VIfE4DE7vY49i"/>
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gif={gifs}/>
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif}/>
         </div>
       </div>
     );
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import Gif from './gif.jsx';
 
-class GifList extends Component {
-  renderList = () => {
-    return this.props.gif.map(gif =>  <Gif id={gif.id} key={gif.id} /> )
-  }
+const GifList = (props) => {
+  return (
+    <div className="gif-list">
+      {props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={props.selectGif} />)}
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="gif-list">
-        {this.renderList()}
-      </div>
-    )
-  }
-}
 
 export default GifList;
